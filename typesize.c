3493a9123326497837c9afaa39c7db480383dda1
jyren2015 <31702289+jyren2015@users.noreply.github.com>
2018-01-06 10:37:53 +0800

    practice
    easy

---
+++ typesize.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Type int has a size of %u bytes.\n",sizeof(int));
+	printf("Type char has a size of %u bytes.\n",sizeof(char));
+	printf("Type double has a size of %u bytes.\n",sizeof(double));
+	return 0;
+}
