47804406c59aa1725eeabaa8e3b87e59aea62d59
任晋宇 <31702289+jyren2015@users.noreply.github.com>
2018-02-16 16:36:11 +0800

    Add files via upload

---
+++ 指针实验.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int a[]={2,7,11,9};
+    int array[4];
+    int* p=&a[0];
+    int s;
+    for(s=0;s<=4;s++){
+   	    array[s]=*p;
+		p=p+1;
+    }
+    printf("%d",array[1]);
+}
+
