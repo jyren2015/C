9f6d50e9777fc441f8795c6b6b11d1cf12d126ea
RJY <31702289+jyren2015@users.noreply.github.com>
2019-12-09 09:34:24 +0800

    Update treeTest.java

--- treeTest.java
+++ treeTest.java
@@ -25,6 +25,55 @@ public class Solution{
             }
         }
     }
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+        if (digits.equals("")) return list;
+        HashMap<Character, String[]> map = new HashMap<>();
+        String[] digit2 = {"a", "b", "c"};
+        String[] digit3 = {"d", "e", "f"};
+        String[] digit4 = {"g", "h", "i"};
+        String[] digit5 = {"j", "k", "l"};
+        String[] digit6 = {"m", "n", "o"};
+        String[] digit7 = {"p", "q", "r", "s"};
+        String[] digit8 = {"t", "u", "v"};
+        String[] digit9 = {"w", "x", "y", "z"};
+        map.put('2', digit2);
+        map.put('3', digit3);
+        map.put('4', digit4);
+        map.put('5', digit5);
+        map.put('6', digit6);
+        map.put('7', digit7);
+        map.put('8', digit8);
+        map.put('9', digit9);
+        Node root = new Node(digits, map, "root");
+        Node move = root;
+        List<String> test = new ArrayList<>();
+        System.out.println("over");
+        System.out.println(root.str_node);
+        String final_str = "";
+        int tag = 0;
+        while (true){
+            if (move.str_node.equals("root") && move.flag1) break;
+            if (move.child == null){
+                test.add(final_str);
+                tag ++;
+                final_str = final_str.substring(0, final_str.length()-1);
+                move = move.father;
+            }
+            else if (move.flag1){
+                final_str = final_str.substring(0, final_str.length()-1);
+                move = move.father;
+            }
+            else {
+                final_str = final_str + move.child[move.flag].str_node;
+                move.flag++;
+                if (move.flag == move.child.length) move.flag1 = true;
+                move = move.child[move.flag-1];
+            }
+        }
+        System.out.println("tag=" + tag);
+        return test;
+    }
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.letterCombinations("23794"));
